cfe26d1475d71cd750ad40bcff62eca88e8af63c
wenzihao <1052951572@qq.com>
2024-03-17 16:45:45 +0800

    add support for intrinsic calibration

--- intrinsicParameter/charucoboardCalibration/get_cam_calibration.py
+++ intrinsicParameter/charucoboardCalibration/get_cam_calibration.py
@@ -2,113 +2,41 @@ import os
 import numpy as np
 import cv2
 
-# ------------------------------
-# ENTER YOUR REQUIREMENTS HERE:
-ARUCO_DICT = cv2.aruco.DICT_4X4_1000
-SQUARES_VERTICALLY = 5
-SQUARES_HORIZONTALLY = 4
-SQUARE_LENGTH = 0.03
-MARKER_LENGTH = 0.022
-# ...
-PATH_TO_YOUR_IMAGES = '/home/wenzihao/Desktop/WandCalibration/testData/charucoboard'
-# ------------------------------
+class IntrinsicCalibration(object):
+    def __init__(
+            self,
+            height,
+            width,
+            square_length,
+            markser_length,
+            aruco_dict=cv2.aruco.DICT_4X4_1000
+        ) -> None:
+        self.dictionary = cv2.aruco.getPredefinedDictionary(aruco_dict)
+        self.board = cv2.aruco.CharucoBoard((height, width), square_length, markser_length, self.dictionary)
+        self.params = cv2.aruco.DetectorParameters()
 
-def calibrate_and_save_parameters():
-    # Define the aruco dictionary and charuco board
-    dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
-    board = cv2.aruco.CharucoBoard((SQUARES_VERTICALLY, SQUARES_HORIZONTALLY), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
-    params = cv2.aruco.DetectorParameters()
-
-    # Load PNG images from folder
-    image_files = [os.path.join(PATH_TO_YOUR_IMAGES, f) for f in os.listdir(PATH_TO_YOUR_IMAGES) if f.endswith(".jpg")]
-    image_files.sort()  # Ensure files are in order
-
-    all_charuco_corners = []
-    all_charuco_ids = []
-
-    for image_file in image_files:
-        image = cv2.imread(image_file)
-        # image=cv2.flip(image,1)
-        marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(image, dictionary, parameters=params)
-        if marker_ids is None:
-            continue
-        charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, image, board)
-        if charuco_retval>3:
-            print({
-                "corners":charuco_corners,
-                "ids":charuco_corners
-            })
-            assert len(charuco_corners)==len(charuco_ids),"len(charuco_corners) != len(charuco_ids)"
-            
-            all_charuco_corners.append(charuco_corners)
-            all_charuco_ids.append(charuco_ids)
-
-    # Calibrate camera
-    retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_charuco_corners, all_charuco_ids, board, image.shape[:2], None, None)
-    print("ret:", retval)
-    print("mtx:\n", camera_matrix.reshape(1,-1).tolist()) # 内参数矩阵
-    print("dist:\n", dist_coeffs.tolist())  # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)
-    # # Save calibration data
-    # np.save('camera_matrix.npy', camera_matrix)
-    # np.save('dist_coeffs.npy', dist_coeffs)
-
-    # # Iterate through displaying all the images
-    # for image_file in image_files:
-    #     image = cv2.imread(image_file)
-    #     undistorted_image = cv2.undistort(image, camera_matrix, dist_coeffs)
-    #     cv2.imshow('Undistorted Image', undistorted_image)
-    #     cv2.waitKey(0)
-
-    # cv2.destroyAllWindows()
-
-calibrate_and_save_parameters()
-exit(0)
-
-def detect_pose(image, camera_matrix, dist_coeffs):
-    # Undistort the image
-    undistorted_image = cv2.undistort(image, camera_matrix, dist_coeffs)
-
-    # Define the aruco dictionary and charuco board
-    dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
-    board = cv2.aruco.CharucoBoard((SQUARES_VERTICALLY, SQUARES_HORIZONTALLY), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
-    params = cv2.aruco.DetectorParameters()
-
-    # Detect markers in the undistorted image
-    marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(undistorted_image, dictionary, parameters=params)
-
-    # If at least one marker is detected
-    if len(marker_ids) > 0:
-        # Interpolate CharUco corners
-        charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, undistorted_image, board)
-
-        # If enough corners are found, estimate the pose
-        if charuco_retval:
-            retval, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(charuco_corners, charuco_ids, board, camera_matrix, dist_coeffs, None, None)
-
-            # If pose estimation is successful, draw the axis
-            if retval:
-                cv2.drawFrameAxes(undistorted_image, camera_matrix, dist_coeffs, rvec, tvec, length=0.1, thickness=15)
-    return undistorted_image
-
-
-def main():
-    # Load calibration data
-    camera_matrix = np.load['camera_matrix.npy']
-    dist_coeffs = np.load['dist_coeffs.npy']
-
-    # Iterate through PNG images in the folder
-    image_files = [os.path.join(PATH_TO_YOUR_IMAGES, f) for f in os.listdir(PATH_TO_YOUR_IMAGES) if f.endswith(".png")]
-    image_files.sort()  # Ensure files are in order
-
-    for image_file in image_files:
-        # Load an image
-        image = cv2.imread(image_file)
-
-        # Detect pose and draw axis
-        pose_image = detect_pose(image, camera_matrix, dist_coeffs)
-
-        # Show the image
-        cv2.imshow('Pose Image', pose_image)
-        cv2.waitKey(0)
-
-main()
+    def __call__(self, image_path_list):
+        all_charuco_corners = []
+        all_charuco_ids = []
+        for image_file in image_path_list:
+            image = cv2.imread(image_file)
+            img_width, img_height = image.shape[:2]
+            marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(image, self.dictionary, parameters=self.params)
+            if marker_ids is None:
+                continue
+            charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, image, self.board)
+            if charuco_retval>3:
+                print({
+                    "corners":charuco_corners,
+                    "ids":charuco_corners
+                })
+                assert len(charuco_corners)==len(charuco_ids),"len(charuco_corners) != len(charuco_ids)"
+                
+                all_charuco_corners.append(charuco_corners)
+                all_charuco_ids.append(charuco_ids)
+        retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_charuco_corners, all_charuco_ids, self.board, image.shape[:2], None, None)
+        return {
+            "image_size":[img_width,img_height],
+            "K":np.squeeze(camera_matrix).tolist(),
+            "dist":np.squeeze(dist_coeffs).tolist()
+        }
--- intrinsicParameter/checkboardCalibration/get_cam_calibration.py
+++ intrinsicParameter/checkboardCalibration/get_cam_calibration.py
@@ -2,79 +2,41 @@ import cv2
 import numpy as np
 import glob
 
-def get_camera_intrinsic(h, w, image_path_list):
-    # h, w: 棋盘格角点规格
-
-    # 设置寻找亚像素角点的参数，采用的停止准则是最大循环次数30和最大误差容限0.001
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
-
-    # 获取标定板角点的在世界坐标系位置， 将世界坐标系建在标定板上，所有点的Z坐标全部为0，所以只需要赋值x和y
-    '''
-    objp:
-    [[0. 0. 0.]
-    [1. 0. 0.]
-    [2. 0. 0.]
-    ...
-    [4. 4. 0.]
-    [5. 4. 0.]
-    [6. 4. 0.]]
-    '''
-    objp = np.zeros((h * w, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:h, 0:w].T.reshape(-1, 2)
-
-    obj_points = []
-    img_points = []
-  
-    for image_path in image_path_list:
-        img = cv2.imread(image_path)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        size = gray.shape[::-1]
-        ret, corners = cv2.findChessboardCorners(gray, (h, w), None)
-        print(ret)
-
-        if ret:
-            obj_points.append(objp)
-
-            # 专门用来获取棋盘图上内角点的精确位置的， 即在原角点的基础上寻找亚像素角点
-            corners2 = cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria) 
-            
-            if [corners2]:
-                img_points.append(corners2)
-            else:
-                img_points.append(corners)
-
-            # cv2.drawChessboardCorners(img, (h, w), corners, ret)
-            # cv2.imshow('img', img)
-            # cv2.waitKey(1000)
-
-    print(len(img_points))
-    # cv2.destroyAllWindows()
-
-    # calibration
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, size, None, None)
-
-    np.set_printoptions(suppress=True)
-    print("ret:", ret)
-    print("mtx:\n", mtx.reshape(1,-1).tolist()) # 内参数矩阵
-    print("dist:\n", dist.tolist())  # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)
-    # print("rvecs:\n", len(rvecs))  # 旋转向量  # 外参数
-    # print("tvecs:\n", tvecs ) # 平移向量  # 外参数
-    # print("-----------------------------------------------------")
-
-    # 用标定结果把空间三维坐标点映射回像素坐标系 再和 标定图片上的亚像素角点坐标 做对比
-    total_error = 0
-    for i in range(len(obj_points)):
-        img_points2, _ = cv2.projectPoints(obj_points[i], rvecs[i], tvecs[i], mtx, dist)
-        error = cv2.norm(img_points[i],img_points2, cv2.NORM_L2)/len(img_points2)
-        total_error += error
-    # 单位是像素
-    print("average error: ", total_error/len(img_points2))
-
-def main():
-    image_path_list = glob.glob("/home/wenzihao/Desktop/WandCalibration/testData/checkerboard/*.jpg")
-    get_camera_intrinsic(5, 4, image_path_list)
-
-if __name__ == '__main__':
-    main()
-
+class IntrinsicCalibration(object):
+    def __init__(
+            self,
+            height,
+            width,
+        ) -> None:
+        # height, width: 棋盘格角点规格
+        self.height=height
+        self.width=width
+        # 获取标定板角点的在世界坐标系位置， 将世界坐标系建在标定板上，所有点的Z坐标全部为0，所以只需要赋值x和y
+        self.objp = np.zeros((self.height * self.width, 3), np.float32)
+        self.objp[:, :2] = np.mgrid[0:self.height, 0:self.width].T.reshape(-1, 2)
+        # 设置寻找亚像素角点的参数，采用的停止准则是最大循环次数30和最大误差容限0.001
+        self.criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
+    def __call__(self, image_path_list):
+        obj_points = []
+        img_points = []
+        for image_path in image_path_list:
+            img = cv2.imread(image_path)
+            img_width, img_height = img.shape[:2]
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            size = gray.shape[::-1]
+            ret, corners = cv2.findChessboardCorners(gray, (self.height, self.width), None)
+            if ret:
+                obj_points.append(self.objp)
+                # 专门用来获取棋盘图上内角点的精确位置的， 即在原角点的基础上寻找亚像素角点
+                corners2 = cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), self.criteria) 
+                if [corners2]:
+                    img_points.append(corners2)
+                else:
+                    img_points.append(corners)
+        # calibration
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, size, None, None)
+        return {
+            "image_size":[img_width,img_height],
+            "K":np.squeeze(mtx).tolist(),
+            "dist":np.squeeze(dist).tolist()
+        }
