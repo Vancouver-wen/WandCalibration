58b9b3c95c1df189721d56e55e4338505bffb44e
wenzihao <1052951572@qq.com>
2024-08-01 10:07:51 +0800

    vis_spread通过颜色来区分可见视野数量

--- visualize/vis_pole_spread.py
+++ visualize/vis_pole_spread.py
@@ -7,23 +7,24 @@ import cv2
 from joblib import Parallel,delayed
 from tqdm import tqdm
 from loguru import logger
+import cmap
 
 from extrinsicParameter.poleDetection.blobDetection import get_cam_list
 from utils.imageConcat import show_multi_imgs
 
-def draw_point_2ds(frame,point_2ds):
+def draw_point_2ds(frame,point_2ds,color):
     frame=cv2.line(
         img=frame,
         pt1=point_2ds[0].astype(np.int32),
         pt2=point_2ds[1].astype(np.int32),
-        color=(0,255,0),
+        color=color.astype(np.uint8).tolist(),
         thickness=1
     )
     frame=cv2.line(
         img=frame,
         pt1=point_2ds[1].astype(np.int32),
         pt2=point_2ds[2].astype(np.int32),
-        color=(0,255,0),
+        color=color.astype(np.uint8).tolist(),
         thickness=1
     )
     for point_2d in point_2ds:
@@ -31,20 +32,23 @@ def draw_point_2ds(frame,point_2ds):
             img=frame,
             center=point_2d.astype(np.int32),
             radius=3,
-            color=(0,0,255),
+            color=color.astype(np.uint8).tolist(),
             thickness=-1
         )
     
 def draw_pole_list(
         frames,
-        pole_list
+        pole_list,
+        colors
     ):
     assert len(frames)==len(pole_list),f"len(frames)!=len(pole_list), check pole detection function"
+    avail_num=np.array([pole is not None for pole in pole_list]).sum()
+    color=colors[avail_num]
     for frame,pole in list(zip(frames,pole_list)):
         if pole is None:
             continue
         point_2ds,indexs=pole
-        draw_point_2ds(frame,point_2ds)
+        draw_point_2ds(frame,point_2ds,color)
 
 def vis_spread(
         cam_num,
@@ -56,9 +60,11 @@ def vis_spread(
         os.mkdir(save_path)
     frame_list=get_cam_list(image_path,cam_num)[0]
     frames=[cv2.imread(frame_path) for frame_path in frame_list]
+    color_bar = cmap.Colormap(["red","blue","green"]) # 以RGBA顺序给出
+    colors=np.flip(color_bar(np.linspace(0,1,len(frames)+1))[:,:3]*255,axis=1) # 转换成BGR
     logger.info(f"visualize pole spread result")
     Parallel(n_jobs=-1,backend="threading")(
-        delayed(draw_pole_list)(frames,pole_list)
+        delayed(draw_pole_list)(frames,pole_list,colors)
         for pole_list in tqdm(pole_lists)
     )
     for step,frame in enumerate(frames):
